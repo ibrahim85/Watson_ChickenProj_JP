7f43a4aea31ff30c6d6598c50dfcb586917a79d7
John Lineberger <38058488+jplineb@users.noreply.github.com>
2019-06-03 11:21:40 -0400

    Finalized Croping mask script, Created forloop cropping mask script

--- code/bounding_box_crop_rotate/bounding_box_crop_rotate_script.py
+++ code/bounding_box_crop_rotate/bounding_box_crop_rotate_script.py
@@ -7,9 +7,7 @@ Created on Fri May 31 11:37:01 2019
 
 import os
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+
 
 
 wrkingdir = os.getcwd()
@@ -24,7 +22,7 @@ contours, hier = cv2.findContours(threshed_img,  cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_SIMPLE)
 
 cnt, area = contours[0], 0
-for c in contours:
+for c in contours: #finds the contour with the largest area
     a = cv2.contourArea(c)
     if a>area:
         cnt=c
@@ -33,92 +31,17 @@ for c in contours:
 
 
 for c in cnt:
-    x,y,w,h = cv2.boundingRect(cnt)
-    gamma = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-#    ellipse = cv2.fitEllipse(c)
-#    cv2.ellipse(img, ellipse, (0,255,0), 2)
-#    
-#    (x,y),radius = cv2.minEnclosingCircle(c)
-#    center = (int(x),int(y))
-#    radius = int(radius)
-#    cv2.circle(img,center,radius,(0,255,0),2)
+    x,y,w,h = cv2.boundingRect(cnt) # calculates the bounding rectangle
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) #Drawis the bounding rectangle
 
 img2 = cv2.resize(img, (1438, 1080))
 cv2.imshow("contours", img2)
  
+
+roi = img[y:y+h, x:x+w] #sets the region of interest to the bounding rectangle
+cv2.imwrite('image_crop.jpg', roi) #crops inputed image
+
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
 
-
-
-
-##crop
-def crop_minAreaRect(img, rect):
-
-    # rotate img
-    angle = rect[2]
-    rows,cols = img.shape[0], img.shape[1]
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
-    img_rot = cv2.warpAffine(img,M,(cols,rows))
-
-    # rotate bounding box
-    rect0 = (rect[0], rect[1], 0.0) 
-    box = cv2.boxPoints(rect0)
-    pts = np.int0(cv2.transform(np.array([box]), M))[0]    
-    pts[pts < 0] = 0
-
-    # crop
-    img_crop = img_rot[pts[1][1]:pts[0][1], 
-                       pts[1][0]:pts[2][0]]
-
-    return img_crop
-
-alpha = crop_minAreaRect(img,)
-
-
-
-#
-#def subimage(image, center, theta, width, height):
-#
-#   ''' 
-#   Rotates OpenCV image around center with angle theta (in deg)
-#   then crops the image according to width and height.
-#   '''
-#
-#   # Uncomment for theta in radians
-#   theta *= 180/np.pi
-#
-#   shape = ( image.shape[1], image.shape[0] ) # cv2.warpAffine expects shape in (length, height)
-#
-#   matrix = cv2.getRotationMatrix2D( center=center, angle=theta, scale=1 )
-#   image = cv2.warpAffine( src=image, M=matrix, dsize=shape )
-#
-#   x = int( center[0] - width/2  )
-#   y = int( center[1] - height/2 )
-#
-#   image = image[ y:y+height, x:x+width ]
-#
-#   return image
-#
-#width = int(rect[1][1] - rect[1][0])
-#height = int(rect[0][1] - rect[0][1])
-#alpha = width/2 + rect[1][0]
-#beta = height/2 + rect[0][1]
-#center = (alpha,beta)
-#theta = int(rect[2])
-#
-#
-#
-#img = subimage(img, center, theta, width, height)
-#
-#cv2.imwrite('path.jpg', img)
-#
-#
-
-################
-#img = cv2.resize(img, (1438, 1080))
-#cv2.imshow("crop", img)
-# 
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
---
+++ code/bounding_box_crop_rotate/forloop_bounding_box_crop_rotate_script.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 11:05:54 2019
+
+@author: jplineb
+"""
+
+import os
+import cv2
+
+wrkingdir = os.getcwd()
+maskroot = './masks'
+
+def cropdamask(fileloc):
+    img = cv2.pyrDown(cv2.imread(fileloc, cv2.IMREAD_UNCHANGED))
+    ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
+                                  0, 255, cv2.THRESH_BINARY)
+    # find contours and get the external one
+    contours, hier = cv2.findContours(threshed_img,  cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+    
+    cnt, area = contours[0], 0
+    for c in contours: #finds the contour with the largest area
+        a = cv2.contourArea(c)
+        if a>area:
+            cnt=c
+            area=a
+            print(area)
+    for c in cnt:
+        x,y,w,h = cv2.boundingRect(cnt) # calculates the bounding rectangle
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) #Drawis the bounding rectangle
+    roi = img[y:y+h, x:x+w] #sets the region of interest to the bounding rectangle
+    cv2.imwrite(fileloc, roi) #crops inputed image
+
+
+
+for root, dirs, files in os.walk(maskroot):
+    for file in files:
+        fileloc = os.path.join(root, file)
+        cropdamask(fileloc)
+        
